3632aa8b75ba0172af48c3b9a954ac06daddb1eb
Artjosh <arthur.heleus.thade@gmail.com>
2024-01-11 17:21:11 -0300

    Delete CHANNEL Messages by Channel ID

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from telethon import TelegramClient
+from telethon.tl.types import PeerChannel
 from rich.console import Console
 from rich.panel import Panel
 import logging
@@ -11,9 +12,9 @@ from rich.progress import track
 console = Console()
 logging.basicConfig(level="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(console=console)])
 
-api_id = 'API_ID'
-api_hash = 'API_HASH'
-channel_username = 'USERNAME'
+api_id = '25926417'  # Substitua com seu api_id
+api_hash = '812a2f35614cccc19b432beeed96ef1e'  # Substitua com seu api_hash
+channel_id = -1002046964190  # Substitua com o ID do seu canal
 
 client = TelegramClient('session_name', api_id, api_hash)
 
@@ -22,13 +23,14 @@ def print_credits():
     panel = Panel(credit_text, expand=False)
     console.print(panel, justify="center")
 
-async def delete_all_messages(channel):
+async def delete_all_messages(channel_id):
+    channel_entity = PeerChannel(channel_id)
     message_ids = []
-    async for message in client.iter_messages(channel):
+    async for message in client.iter_messages(channel_entity):
         message_ids.append(message.id)
         if len(message_ids) >= 100:
             try:
-                await client.delete_messages(channel, message_ids)
+                await client.delete_messages(channel_entity, message_ids)
                 console.log(f"[bold green]Deleted {len(message_ids)} messages.[/bold green]")
                 message_ids = []
                 await asyncio.sleep(2)
@@ -37,7 +39,7 @@ async def delete_all_messages(channel):
 
     if message_ids:
         try:
-            await client.delete_messages(channel, message_ids)
+            await client.delete_messages(channel_entity, message_ids)
             console.log(f"[bold green]Deleted {len(message_ids)} messages.[/bold green]")
         except Exception as e:
             console.log(f"[bold red]Error in final bulk deletion: {e}[/bold red]")
@@ -49,7 +51,7 @@ async def main():
     try:
         for _ in track(range(100), description="Processing..."):
             await asyncio.sleep(0.1)  # Simulated task
-        await delete_all_messages(channel_username)
+        await delete_all_messages(channel_id)
     finally:
         await client.disconnect()
         console.log("[bold magenta]Client disconnected.[/bold magenta]")
